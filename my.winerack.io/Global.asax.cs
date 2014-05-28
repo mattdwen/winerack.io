77e70d0bdea239a272523c0040529c87d02f3792
Matt Dwen <matt@group6.co.nz>
2014-05-29 08:33:37 +1200

    Production deploy DB migrations

--- my.winerack.io/Global.asax.cs
+++ my.winerack.io/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http;
+﻿using System.Configuration;
+using System.Data.Entity.Migrations;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -8,6 +10,12 @@ namespace winerack {
 	public class MvcApplication : System.Web.HttpApplication {
 
 		protected void Application_Start() {
+			if (bool.Parse(ConfigurationManager.AppSettings["MigrateDatabaseToLatestVersion"])) {
+				var configuration = new winerack.Migrations.Configuration();
+				var migrator = new DbMigrator(configuration);
+				migrator.Update();
+			}
+
 			AreaRegistration.RegisterAllAreas();
 			GlobalConfiguration.Configure(WebApiConfig.Register);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
