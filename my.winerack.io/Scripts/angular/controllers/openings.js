226a048daf1c26a0da84433ed2c3b3777f18dead
Matt Dwen <matt@group6.co.nz>
2015-03-03 15:31:03 +1300

    Open a bottle search style and varietal

--- my.winerack.io/Scripts/angular/controllers/openings.js
+++ my.winerack.io/Scripts/angular/controllers/openings.js
@@ -1,69 +1,82 @@
 ﻿var openingsApp = angular.module('openingsApp', ['bottleService', 'ui.utils', 'ui.select2']);
 
 openingsApp.controller('StartCtrl', function ($scope, Bottle) {
-    $scope.select2Options = {
-        allowClear: true
-    };
+  $scope.select2Options = {
+    allowClear: true
+  };
 
-    $scope.bottles = Bottle.query();
+  $scope.styles = [];
+  $scope.varietals = [];
 
-    $scope.filter = {
-        drinking: '',
-        keywords: '',
-        style: '',
-        varietal: ''
-    };
+  $scope.bottles = Bottle.query(function (bottles) {
+    var styles = [];
+    var varietals = [];
+    angular.forEach(bottles, function (bottle) {
+      styles = $.merge(styles, bottle.Styles);
+      varietals = $.merge(varietals, bottle.Varietals);
+    });
 
-    $scope.search = function (bottle) {
-        var style = false,
-            varietal = false,
-            keywords = false,
-            drinking = false;
+    $scope.styles = styles;
+    $scope.varietals = varietals;
+  });
 
-        // Drinking
-        if ($scope.filter.drinking == '') {
-            drinking = true;
-        } else {
-            var yearNow = new Date().getFullYear(),
-                yearStart = bottle.Vintage + bottle.CellarMin,
-                yearEnd = bottle.Vintage + bottle.CellarMax;
+  $scope.filter = {
+    drinking: '',
+    keywords: '',
+    style: '',
+    varietal: ''
+  };
 
-            if ($scope.filter.drinking == 'now' && yearStart <= yearNow && yearEnd >= yearNow) {
-                drinking = true;
-            } else if ($scope.filter.drinking == 'over' && bottle.Vintage > 0 && yearEnd < yearNow) {
-                drinking = true;
-            } else if ($scope.filter.drinking == 'now' && bottle.Vintage == undefined) {
-                drinking = true;
-            }
-        }
+  $scope.search = function (bottle) {
+    var style = false,
+        varietal = false,
+        keywords = false,
+        drinking = false;
 
-        // Keywords
-        if ($scope.filter.keywords == '') {
-            keywords = true;
-        } else {
-            var q = $scope.filter.keywords,
-                desc = bottle.Description.toLowerCase(),
-                vineyard = bottle.Vineyard.toLowerCase();
+    // Drinking
+    if ($scope.filter.drinking == '') {
+      drinking = true;
+    } else {
+      var yearNow = new Date().getFullYear(),
+          yearStart = bottle.Vintage + bottle.CellarMin,
+          yearEnd = bottle.Vintage + bottle.CellarMax;
 
-            if (desc.indexOf(q) > -1 || vineyard.indexOf(q) > -1) {
-                keywords = true;
-            }
-        }
-        
-        // Style
-        if ($scope.filter.style == '') {
-            style = true;
-        } else if ($scope.filter.style == bottle.VarietalStyle) {
-            style = true;
-        }
+      if ($scope.filter.drinking == 'now' && yearStart <= yearNow && yearEnd >= yearNow) {
+        drinking = true;
+      } else if ($scope.filter.drinking == 'over' && bottle.Vintage > 0 && yearEnd < yearNow) {
+        drinking = true;
+      } else if ($scope.filter.drinking == 'now' && bottle.Vintage == undefined) {
+        drinking = true;
+      }
+    }
 
-        // Varietal
-        if ($scope.filter.varietal == '') {
-            varietal = true;
-        } else if ($scope.filter.varietal == bottle.Varietal) {
-            varietal = true;
-        }
+    // Keywords
+    if ($scope.filter.keywords == '') {
+      keywords = true;
+    } else {
+      var q = $scope.filter.keywords,
+          desc = bottle.Description.toLowerCase(),
+          vineyard = bottle.Vineyard.toLowerCase();
 
-        return (varietal && style && drinking && keywords);
-    };
+      if (desc.indexOf(q) > -1 || vineyard.indexOf(q) > -1) {
+        keywords = true;
+      }
+    }
+
+    // Style
+    if ($scope.filter.style == '') {
+      style = true;
+    } else if (bottle.Styles.indexOf($scope.filter.style) >= 0) {
+      style = true;
+    }
+
+    // Varietal
+    if ($scope.filter.varietal == '') {
+      varietal = true;
+    } else if (bottle.Varietals.indexOf($scope.filter.varietal) >= 0) {
+      varietal = true;
+    }
+
+    return (varietal && style && drinking && keywords);
+  };
 });
