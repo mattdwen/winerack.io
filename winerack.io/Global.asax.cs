8030dbceac5a0f78d6fcdf10a50d73cc9597b258
Matt Dwen <matt@group6.co.nz>
2015-03-17 16:04:27 +1300

    Raygun integration

--- winerack.io/Global.asax.cs
+++ winerack.io/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Mindscape.Raygun4Net;
+using System.Configuration;
 using System.Data.Entity.Migrations;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -7,11 +8,28 @@ using System.Web.Routing;
 
 namespace winerack {
 
-	public class MvcApplication : System.Web.HttpApplication {
+	public class MvcApplication : System.Web.HttpApplication
+    {
 
-		public const string ADMINISTRATOR_GROUP = "Administrators";
+        #region Constants
 
-		protected void Application_Start() {
+        public const string ADMINISTRATOR_GROUP = "Administrators";
+
+        #endregion Constants
+
+        #region Declarations
+
+        RaygunClient client;
+
+        #endregion Declarations
+
+        protected void Application_Start() {
+            var config = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration(null);
+            var raygunKey = config.AppSettings.Settings["raygun:apiKey"];
+            if (raygunKey != null) {
+                client = new RaygunClient(raygunKey.Value);
+            }
+
 			if (bool.Parse(ConfigurationManager.AppSettings["MigrateDatabaseToLatestVersion"])) {
 				var configuration = new winerack.Migrations.Configuration();
 				var migrator = new DbMigrator(configuration);
@@ -24,5 +42,13 @@ namespace winerack {
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
 		}
+
+        protected void Application_Error()
+        {
+            if (client != null) {
+                var exception = Server.GetLastError();
+                client.SendInBackground(exception);
+            }
+        }
 	}
 }
