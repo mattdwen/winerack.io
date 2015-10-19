28ebe0d1b84dc2c141ead669c248e812235dee5e
Matt Dwen <matt@group6.co.nz>
2015-10-19 17:37:42 +1300

    Some kind of structure ¯\_(ツ)_/¯

---
+++ winerack/gruntfile.js
@@ -0,0 +1,73 @@
+﻿module.exports = function (grunt) {
+
+  // Configuration
+  // --------------------------------------------------------------------------
+
+  grunt.initConfig({
+
+    // Clean
+    //
+    clean: {
+      lib: ['./wwwroot/lib/']
+    },
+
+    // Copy files
+    //
+    copy: {
+      bower: {
+        expand: true,
+        cwd: './bower_components/',
+        src: [
+          'bootstrap/dist/js/bootstrap.*',
+          'jquery/dist/jquery.*',
+          'jquery-validation/dist/jquery.validate*',
+          'jquery-validation-unobtrusive/*.js'
+        ],
+        dest: './wwwroot/lib/'
+      },
+      fonts: {
+        expand: true,
+        cwd: './bower_components/font-awesome/fonts',
+        src: '*',
+        dest: './wwwroot/lib/font-awesome'
+      },
+    },
+    
+    // Compile Sass stylesheets
+    //
+    sass: {
+      options: {
+        loadPath: 'bower_components'
+      },
+      main: {
+        files: [
+        {
+          expand: true,
+          cwd: './Styles',
+          src: '*.{scss,sass}',
+          dest: './wwwroot/css',
+          ext: '.css'
+        }]
+      }
+    }
+    
+  });
+
+  // Load tasks
+  // --------------------------------------------------------------------------
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+
+
+  // Tasks
+  // --------------------------------------------------------------------------
+
+  grunt.registerTask('build', [
+    'clean',
+    'copy',
+    'sass'
+  ]);
+};
